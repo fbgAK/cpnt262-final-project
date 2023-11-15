f9982af6b349aa19647604d511eeaa04ea2820e9
Sterling Smith <sterling12311@gmail.com>
2023-11-15 09:48:46 -0700

    added first image to the gallery page

--- src/lib/data.js
+++ src/lib/data.js
@@ -3,20 +3,75 @@
 // Array of objects containing data for each card
 export const cardsData = [
     {
-        // Burger card data
-        title: 'Burger',
-        description: 'Ingredients',
-        src: "../static/images/burger.jpg", // Image src
-        alt: 'Hamburger with fries', // ALT text
-        style: 'max-width: 100%; height: auto;' // Styling for Cake image
+        title: 'Hamburger and fries',
+        description: 'Hamburger with crisp french fries',
+        imgURL: '../static/images/burger.jpg',
+        altImg: 'Hamburger and fries',
+        externalURL: '',
+        style: 'max-width: 100%; height: auto;'
     },
     {
-        // Ice Cream card data
-        title: 'Ice Cream',
-        description: 'Ingredients',
-        imgURL: 'images/icecream.png', // Image URL for Ice Cream card
-        altImg: 'Ice Cream pic', // ALT text for the Ice Cream image
-        externalURL: "https://barefeetinthekitchen.com/homemade-ice-cream-recipe/#wprm-recipe-container-23944", // Clickable external URL for the Ice Cream card
-        style: 'max-width: 100%; height: auto;' // Styling for Ice Cream image
+        title: 'Strawberry Shortcake',
+        description: 'Picture here.',
+        imgURL: 'images/strawberry_shortcake.jpg',
+        altImg: 'Strawberry Shortcake',
+        externalURL: 'url',
+        style: 'max-width: 100%; height: auto;'
+    },
+    {
+        title: 'Vanilla Ice Cream',
+        description: 'Classic vanilla ice cream in a cone.',
+        imgURL: 'images/vanilla_ice_cream.jpg',
+        altImg: 'Vanilla Ice Cream',
+        externalURL: 'https://example.com/vanilla-ice-cream',
+        style: 'max-width: 100%; height: auto;'
+    },
+    {
+        title: 'Blueberry Muffins',
+        description: 'Delicious blueberry muffins with a crumbly top.',
+        imgURL: 'images/blueberry_muffins.jpg',
+        altImg: 'Blueberry Muffins',
+        externalURL: 'https://example.com/blueberry-muffins',
+        style: 'max-width: 100%; height: auto;'
+    },
+    {
+        title: 'Pancakes',
+        description: 'Fluffy pancakes stacked high with syrup.',
+        imgURL: 'images/pancakes.jpg',
+        altImg: 'Pancakes',
+        externalURL: 'https://example.com/pancakes',
+        style: 'max-width: 100%; height: auto;'
+    },
+    {
+        title: 'Cheesecake',
+        description: 'Creamy cheesecake topped with fruit compote.',
+        imgURL: 'images/cheesecake.jpg',
+        altImg: 'Cheesecake',
+        externalURL: 'https://example.com/cheesecake',
+        style: 'max-width: 100%; height: auto;'
+    },
+    {
+        title: 'Chocolate Chip Cookies',
+        description: 'Homemade chocolate chip cookies, warm and gooey.',
+        imgURL: 'images/chocolate_chip_cookies.jpg',
+        altImg: 'Chocolate Chip Cookies',
+        externalURL: 'https://example.com/chocolate-chip-cookies',
+        style: 'max-width: 100%; height: auto;'
+    },
+    {
+        title: 'Fruit Salad',
+        description: 'Fresh and colorful fruit salad.',
+        imgURL: 'images/fruit_salad.jpg',
+        altImg: 'Fruit Salad',
+        externalURL: 'https://example.com/fruit-salad',
+        style: 'max-width: 100%; height: auto;'
+    },
+    {
+        title: 'Cupcakes',
+        description: 'Assorted cupcakes with vibrant frosting.',
+        imgURL: 'images/cupcakes.jpg',
+        altImg: 'Cupcakes',
+        externalURL: 'https://example.com/cupcakes',
+        style: 'max-width: 100%; height: auto;'
     },
 ];
