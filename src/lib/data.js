670a3334aa6cbdcf70e9533847769d73ecda9bcc
Sterling Smith <sterling12311@gmail.com>
2023-11-24 15:15:28 -0700

    added larger images to cards

--- src/lib/data.js
+++ src/lib/data.js
@@ -16,7 +16,7 @@ export const cardsData = [
     description: 'Chicken with delicious veggies',
     detailed_description: 'Here is an easy dinner idea that your whole family is sure to love. It is easy, delicious, healthy, and inexpensive. What more could you need? If you are hungry for takeout, skip the wait (and the delivery fee) and just whip this up. It has got an easy sauce made mostly out of pantry ingredients, is packed with fresh veggies, and the whole thing comes together faster than take-out. If you have not made stir-fry at home before, give this recipe a try and you may not order out again.',
     imgURL: 'images/easy-dinner-ideas-chicken-and-veggies-stir-fry-6452a10aa5c3f.jpg',
-    largeImgURL: 'images/chicken-veggies.jpg',
+    largeImgURL: '/images/chicken-veggies.jpg',
     altImg: 'Chicken and Veggies',
   },
   {
@@ -25,7 +25,7 @@ export const cardsData = [
     description: 'A delicious chicken dish!',
     detailed_description: 'Chicken Marsala is a simple dish of boneless chicken thats been pounded thin, breaded, and sautéed, served with a Marsala-wine sauce. Its a classic of Italian-American cuisine that was incredibly popular in the U.S. A lot of the flavor comes from Marsala, which is a dry fortified wine similar to sherry, made in Sicily. If you are looking for ways to make chicken a little less boring, put this recipe in your pocket. The classic creamy, savory sauce is easy to make and so good you will want to drizzle it over everything. And it will pair with some steamed vegetables or your favorite side dish for a complete meal.',
     imgURL: 'images/easy-dinner-ideas-chicken-marsala-1642715952.jpg',
-    largeImgURL: '',
+    largeImgURL: '/images/chicken-marsla.jpg',
     altImg: 'Chicken Marsala',
   },
   {
@@ -34,7 +34,7 @@ export const cardsData = [
     description: 'Easy gnocchi with creamed spinach sauce',
     detailed_description: 'Got 10 minutes? Besides that, all you need for this 10-minute meal is a pack of gnocchi, frozen spinach, some cream, some cheese, and a few pantry spices. Serve it with a glass of wine and you have got a fancy dinner for two that tastes like it took a lot more work than it actually did.',
     imgURL: 'images/easy-dinner-ideas-gnocchi-with-creamed-spinach-64529b8bd78f3.jpg',
-    largeImgURL: '',
+    largeImgURL: '/images/gnocchi.jpg',
     altImg: 'Gnocchi with Spinach',
   },
   {
@@ -43,7 +43,7 @@ export const cardsData = [
     description: 'The perfect soup for a cold day',
     detailed_description: 'This easy, 25-minute recipe gives you all the flavor of the worlds favorite casserole in the worlds favorite comfort-food form! Broken noodles simmer in a tomato broth with Italian sausage and spinach, and the whole thing is capped off with a big dollop of blended cheese. Though it is an easy-weeknight meal (and a real crowd pleaser) we also like to put this together for a big Sunday supper with a side of garlic bread. ',
     imgURL: 'images/easy-dinner-ideas-lasagna-soup-64529c039668f.jpg',
-    largeImgURL: '',
+    largeImgURL: '/images/lasagna-soup.jpg',
     altImg: 'Lasagna Soup',
   },
   {
@@ -52,7 +52,7 @@ export const cardsData = [
     description: 'Migas with Chorizo sausage',
     detailed_description: 'This simple Mexican country meal makes a great breakfast or dinner. While we love practically any kind of breakfast, there is a special place in our hearts for this Mexican-style scrambled egg dish. Migas, which means "crumbs" in Spanish, is a dish of leftovers. There is nothing for a fast dinner like making breakfast for dinner! This recipe goes all out—scrambled eggs with corn tortillas, spicy chorizo, and plenty of toppings—and still takes just 15 minutes to make. While we love practically any kind of breakfast, there is a special place in our hearts for this Mexican-style scrambled egg dish. Migas, which means "crumbs" in Spanish, is a dish of leftovers. Take a few of the tortillas you did not use for last nights tacos, and mix them with a bit of the leftover chorizo. Scramble in some eggs, and top them with whatever toppings you still have on hand. What you end up with is a meal that is so much more than the sum of its parts. Give it a try',
     imgURL: 'images/easy-dinner-ideas-migas-with-chorizo-64529e105c15f.jpg',
-    largeImgURL: '',
+    largeImgURL: '/image/chorizo.jpg',
     altImg: 'Migas with Chorizo',
   },
   {
@@ -61,7 +61,7 @@ export const cardsData = [
     description: 'Juicy steak recipe!',
     detailed_description: 'Salisbury steak—that classic American knife-and-fork ground beef dish is more than just a bun-less burger. The dish has been a mainstay on American tables since the late 1800s, when it was invented by doctor (and meat enthusiast) James Salisbury. It is delicious, easy to make, and oh-so comforting.',
     imgURL: 'images/easy-dinner-ideas-salisbury-steak-1642715810.jpg',
-    largeImgURL: '',
+    largeImgURL: '/images/steak.jpg',
     altImg: 'Salisbury Steak',
   },
   {
@@ -70,7 +70,7 @@ export const cardsData = [
     description: 'Salmon and Creamed Spinach',
     detailed_description: 'This fast weeknight pasta is elegant and easy! One of the most versatile ingredients you can keep on hand is a bag of frozen spinach. We have found so many ways to use it, from easy weeknight dinners to party foods and more. This may be one of our favorites. If you are craving a tasty pasta dish that is a little more grown-up than jarred red sauce, give this dish a whirl. We are pretty sure it will become a regular part of your dinner plans.',
     imgURL: 'images/easy-dinner-ideas-salmon-and-creamed-spinach-fettuccini-6452a00904241.jpg',
-    largeImgURL: '',
+    largeImgURL: '/images/salmon.jpg',
     altImg: 'Salmon and Creamed Spinach',
   },
   {
@@ -79,7 +79,7 @@ export const cardsData = [
     description: 'Tuna Melt',
     detailed_description: 'The underrated tuna melt, that staple of diners and lunch plates everywhere, deserves to be making a big time comeback. First, it is as tasty as can be. Second, it is easy to make, and third, you probably have most of the ingredients in your fridge right now. What are you waiting for?  We took that classic tuna salad and gave it a makeover with lots of fresh greens and herbs (celery, dill, scallions) and just the right amount — not too much — of mayo. Then we layered lots of grated Cheddar on top (so it will melt), and topped it with a technique for pan-toasting bread that, as you can see, looks as good as it tastes. Serve it with your favorite chips and a pickle spear for the ultimate lunch-in-a-diner-but-at-home experience.',
     imgURL: 'images/easy-dinner-ideas-tuna-melt-64529b179c289.jpg',
-    largeImgURL: '',
+    largeImgURL: '/images/tuna.jpg',
     altImg: 'Tuna Melt',
   },
 ];
