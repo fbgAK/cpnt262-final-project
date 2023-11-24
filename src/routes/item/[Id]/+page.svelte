8723171b72438865ad567d6b9618c2a305f48049
Sterling Smith <sterling12311@gmail.com>
2023-11-24 15:32:31 -0700

    sized larger images on secondary pages

--- src/routes/item/[Id]/+page.svelte
+++ src/routes/item/[Id]/+page.svelte
@@ -14,12 +14,16 @@
 {#if data.title}
 <ItemCard title={data.title} detailed_description={data.detailed_description} largeImgURL={data.largeImgURL} altImg={data.altImg} />
 {:else}
-<p>ITEM NOT FOUND</p>
+<!-- <p>ITEM NOT FOUND</p> -->
 {/if}
 
 <style>
   h1 {
     color: rgb(194, 106, 23);
     font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+
+  :global(img)  {
+    width: 100vh;
   }
 </style>
